9aab28dae5eb19e4387550a9152694a8d25e765a
kuzyapro322 <kir888555@mail.ru>
2025-09-11 18:31:37 +0700

    Книги.js

--- lab6/books.js
+++ lab6/books.js
@@ -1,1 +1,82 @@
-
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон Дао и дэ',
+        publicationYear: 2022,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 1200,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 950,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 600,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 1925,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Эрнест Хемингуэй',
+        name: 'Старик и море',
+        publicationYear: 1952,
+        price: 500,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 800,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джон Толкин',
+        name: 'Властелин колец: Братство кольца',
+        publicationYear: 1954,
+        price: 900,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 700,
+        bestseller: false,
+        inStock: true
+    }
+];
